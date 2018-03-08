b0d9eadc23de763aa89e7f8775431e6abfdd2992
Sean Huber <seanhuber@seanhuber.com>
2018-03-08 08:43:03 -0800

    tests not using Rails' configured timezone (utc)

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+#ruby=ruby-2.2.4
+#ruby-gemset=tablifier
+
 # Declare your gem's dependencies in log_inspector.gemspec.
 # Bundler will treat runtime dependencies like base dependencies, and
 # development dependencies will be added by default to the :development group.
--- spec/tablifier_spec.rb
+++ spec/tablifier_spec.rb
@@ -6,7 +6,7 @@ describe Tablifier do
       integer: 345,
       float: 3.14,
       date: Date.parse('2016-07-13'),
-      datetime: Time.parse('2016-04-30 11:30am'),
+      datetime: Time.zone.parse('2016-04-30 11:30am'),
       true_boolean: true,
       false_boolean: false,
       string: 'This is a string!',
@@ -14,7 +14,7 @@ describe Tablifier do
       symbol: :my_symbol,
       encrypted_attribute: 'fwoifje;aoiwjfe'
     }
-    expect(Tablifier::Base.tablify(h).squish.gsub('> <', '><')).to eq("<table class='tablifier'><thead><th colspan=\"2\"><div class='obj-type'><span><label>Record Type:</label><span class='val'>Hash</span></span></div></th></thead><tr><td>integer</td><td class='attr-val integer'>345</td></tr><tr><td>float</td><td class='attr-val float'>3.14</td></tr><tr><td>date</td><td class='attr-val date'>Jul 13, 2016</td></tr><tr><td>datetime</td><td class='attr-val date-time'>Apr 30, 2016; 11:30am (CDT)</td></tr><tr><td>true_boolean</td><td class='attr-val boolean true'>True</td></tr><tr><td>false_boolean</td><td class='attr-val boolean false'>False</td></tr><tr><td>string</td><td class='attr-val string'>&quot;This is a string!&quot;</td></tr><tr><td>nil</td><td class='attr-val nil'>nil</td></tr><tr><td>symbol</td><td class='attr-val symbol'>my_symbol</td></tr><tr><td>encrypted_attribute</td><td class='attr-val'>&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;</td></tr></tbody></table>")
+    expect(Tablifier::Base.tablify(h).squish.gsub('> <', '><')).to eq("<table class='tablifier'><thead><th colspan=\"2\"><div class='obj-type'><span><label>Record Type:</label><span class='val'>Hash</span></span></div></th></thead><tr><td>integer</td><td class='attr-val integer'>345</td></tr><tr><td>float</td><td class='attr-val float'>3.14</td></tr><tr><td>date</td><td class='attr-val date'>Jul 13, 2016</td></tr><tr><td>datetime</td><td class='attr-val date-time'>Apr 30, 2016; 11:30am (UTC)</td></tr><tr><td>true_boolean</td><td class='attr-val boolean true'>True</td></tr><tr><td>false_boolean</td><td class='attr-val boolean false'>False</td></tr><tr><td>string</td><td class='attr-val string'>&quot;This is a string!&quot;</td></tr><tr><td>nil</td><td class='attr-val nil'>nil</td></tr><tr><td>symbol</td><td class='attr-val symbol'>my_symbol</td></tr><tr><td>encrypted_attribute</td><td class='attr-val'>&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;</td></tr></tbody></table>")
   end
 
   it 'should be able to generate html tables for an ActiveRecord object' do
@@ -22,8 +22,8 @@ describe Tablifier do
       email_address: 'my_email_234234@gmail.com',
       encrypted_password: '2q9292byf3phnpc298n',
       sign_in_count: '14',
-      created_at: Time.parse('2016-04-30 11:30am'),
-      updated_at: Time.parse('2016-05-18 10:17pm'),
+      created_at: Time.zone.parse('2016-04-30 11:30am'),
+      updated_at: Time.zone.parse('2016-05-18 10:17pm'),
       first_name: 'Jane',
       last_name: 'Doe',
       middle_name: nil,
@@ -31,6 +31,6 @@ describe Tablifier do
       registered: true,
       admin: false
     )
-    expect(Tablifier::Base.tablify(user).squish.gsub('> <', '><')).to eq("<table class='tablifier'><thead><th colspan=\"2\"> Jane Doe <div class='obj-type'><span><label>Record Type:</label><span class='val'>User</span></span></div></th></thead><tr><td>id</td><td class='attr-val nil'>nil</td></tr><tr><td>email_address</td><td class='attr-val string'>&quot;my_email_234234@gmail.com&quot;</td></tr><tr><td>encrypted_password</td><td class='attr-val'>&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;</td></tr><tr><td>sign_in_count</td><td class='attr-val integer'>14</td></tr><tr><td>created_at</td><td class='attr-val date-time'>Apr 30, 2016; 4:30pm (UTC)</td></tr><tr><td>updated_at</td><td class='attr-val date-time'>May 19, 2016; 3:17am (UTC)</td></tr><tr><td>first_name</td><td class='attr-val string'>&quot;Jane&quot;</td></tr><tr><td>last_name</td><td class='attr-val string'>&quot;Doe&quot;</td></tr><tr><td>middle_name</td><td class='attr-val nil'>nil</td></tr><tr><td>birth_date</td><td class='attr-val date'>Jul 13, 1984</td></tr><tr><td>registered</td><td class='attr-val boolean true'>True</td></tr><tr><td>admin</td><td class='attr-val boolean false'>False</td></tr></tbody></table>")
+    expect(Tablifier::Base.tablify(user).squish.gsub('> <', '><')).to eq("<table class='tablifier'><thead><th colspan=\"2\"> Jane Doe <div class='obj-type'><span><label>Record Type:</label><span class='val'>User</span></span></div></th></thead><tr><td>id</td><td class='attr-val nil'>nil</td></tr><tr><td>email_address</td><td class='attr-val string'>&quot;my_email_234234@gmail.com&quot;</td></tr><tr><td>encrypted_password</td><td class='attr-val'>&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;</td></tr><tr><td>sign_in_count</td><td class='attr-val integer'>14</td></tr><tr><td>created_at</td><td class='attr-val date-time'>Apr 30, 2016; 11:30am (UTC)</td></tr><tr><td>updated_at</td><td class='attr-val date-time'>May 18, 2016; 10:17pm (UTC)</td></tr><tr><td>first_name</td><td class='attr-val string'>&quot;Jane&quot;</td></tr><tr><td>last_name</td><td class='attr-val string'>&quot;Doe&quot;</td></tr><tr><td>middle_name</td><td class='attr-val nil'>nil</td></tr><tr><td>birth_date</td><td class='attr-val date'>Jul 13, 1984</td></tr><tr><td>registered</td><td class='attr-val boolean true'>True</td></tr><tr><td>admin</td><td class='attr-val boolean false'>False</td></tr></tbody></table>")
   end
 end
